fef2ffda2b27dd014a1463418f2855c251705df9
Chris Schneider <chris@christopher-schneider.com>
2014-03-16 22:44:13 +0000

    Setup a cabal file, and comment out most of Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,33 +1,34 @@
 module Main where
 
 import System.IO
-import Network
-import Parser
-import Executor
+-- import Network
+-- import Parser
+-- import Executor
 
-main::IO()
-main = startServer
+main :: IO ()
+main = putStrLn "Main Called"
+-- main = startServer
 
-startServer = withSocketsDo $ 
-                do
-                  socket <- listenOn $ PortNumber port
-                  connectionLoop socket
-                  sClose socket    -- No way to get here...
-                where
-                port = 9090
-
-connectionLoop socket = do
-                          connection <- accept socket
-                          command <- readInput connection
-                          replayInput connection command
-                          flushOutput connection
-                          closeConnection connection
-                          connectionLoop socket
-                                                     
-readInput (handle, hostname, portnumber) = hGetLine handle          
-
-replayInput (handle,_,_) text = hPutStrLn handle text                                      
-
-flushOutput (handle,_,_) = hFlush handle
-
-closeConnection (handle,_,_) = hClose handle
+-- startServer = withSocketsDo $ 
+--                 do
+--                   socket <- listenOn $ PortNumber port
+--                   connectionLoop socket
+--                   sClose socket    -- No way to get here...
+--                 where
+--                 port = 9090
+-- 
+-- connectionLoop socket = do
+--                           connection <- accept socket
+--                           command <- readInput connection
+--                           replayInput connection command
+--                           flushOutput connection
+--                           closeConnection connection
+--                           connectionLoop socket
+--                                                      
+-- readInput (handle, hostname, portnumber) = hGetLine handle          
+-- 
+-- replayInput (handle,_,_) text = hPutStrLn handle text                                      
+-- 
+-- flushOutput (handle,_,_) = hFlush handle
+-- 
+-- closeConnection (handle,_,_) = hClose handle
